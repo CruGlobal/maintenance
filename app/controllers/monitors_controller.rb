b35c241f0e9e16c6163988423b9177d91dcd33c5
Josh Starcher <josh.starcher@gmail.com>
2015-10-01 08:50:06 -0400

    Remove reference to newrelic

--- app/controllers/monitors_controller.rb
+++ app/controllers/monitors_controller.rb
@@ -1,8 +1,5 @@
 class MonitorsController < ApplicationController
-  skip_before_action :ensure_login
-  skip_before_action :ensure_setup_finished
   layout nil
-  newrelic_ignore
 
   def lb
     render text: 'OK'
