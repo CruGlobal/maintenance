eafccaf824f6bef9ab3123f4500cb8ebd7f7c3c1
Josh Starcher <josh.starcher@gmail.com>
2015-10-05 12:27:29 -0400

    Use correct environment

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 require 'redis'
 require 'redis/namespace'
 
-Redis.current = Redis::Namespace.new("maintenance:#{Rails.env}", redis: Redis.new(host: ENV.fetch('REDIS_PORT_6379_TCP_ADDR')))
+Redis.current = Redis::Namespace.new("maintenance:#{ENV.fetch('ENVIRONMENT')}", redis: Redis.new(host: ENV.fetch('REDIS_PORT_6379_TCP_ADDR')))
