3e0d2dadbb980291ab61da06d0aed40f27a136a5
Justin Sabelko <justin.sabelko@cru.org>
2019-06-20 16:02:19 -0500

    Fix config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,8 +79,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   # config.log_formatter = ::Logger::Formatter.new
-  # config.logger = ActiveSupport::TaggedLogging.new(Logger::Syslog.new("maintenance-#{ENV['ENVIRONMENT']}",
-                                                                      Syslog::LOG_LOCAL7))
+  # config.logger = ActiveSupport::TaggedLogging.new(Logger::Syslog.new("maintenance-#{ENV['ENVIRONMENT']}", Syslog::LOG_LOCAL7))
   # config.log_tags = [->(request) { "ReqID:#{request.uuid}" }]
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
