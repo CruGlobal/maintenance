449311871839512c0b16f2eb5ae1762e63cdc253
Justin Sabelko <justin.sabelko@cru.org>
2024-02-21 16:20:23 -0600

    Upgrade to Rails 7

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "jquery"
+import "bootstrap"
+import "chosen-js"
+import "tether"
+import "./validations"
+
+var event = new CustomEvent("modulesLoaded");
+document.dispatchEvent(event);
